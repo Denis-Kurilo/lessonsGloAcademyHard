23858cbb5dda4aa552d8289d82c769ed1192b9e4
Denis Kurilo <kdb2015@mail.ru>
2020-06-05 16:54:45 +0300

    added lesson02

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 
+let num = 266219; 
+let arr = ('' + num).split('');
+let result = arr[0] * arr[1] * arr[2] * arr[3] * arr[4] * arr[5];
+let exponentiation = result ** 3;
+let number = ('' + exponentiation).split('');
+
+	number = arr[0] * arr[1];
+
+	console.log(result);
+	console.log(exponentiation);
+	console.log(number);
+
+
+/*let num = 266219; 
+let arr = ('' + num).split('');
+
+console.log(arr)
+
+for(i = 0; i < arr.length; i++) {
+
+}*/
+
