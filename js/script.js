8f9ad8cd87b31ebb5fcb75b666b142f28e614d26
Kurilo Denis <webprogram2017@mail.ru>
2020-06-09 00:52:37 +0300

    add lesson03

--- js/script.js
+++ js/script.js
@@ -1,44 +1,63 @@
+/*
+Переменная lang может принимать 2 значения: 'ru' 'en'.
+Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке. Решите задачу
+через if, 
+через switch-case 
+через многомерный массив без ифов и switch.
+*/
 
-let num = 266219; 
+/*let lang = prompt('Введите значение ru или en'),
+	daysOfTheWeekRu = ['Понедельник','Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+	daysOfTheWeekEn = ['Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];*/
 
-	num = ('' + num).split('');
-let reducer = (accumulator, currentValue) => accumulator * currentValue;
-let sum = num.reduce(reducer);
-
-let exponentiation = sum ** 3;
-let number = ('' + exponentiation).split('');
-number = number[0] + number[1];
-	
-
-console.log(sum);
-console.log(exponentiation);
-console.log(parseInt(number));
-
-
-
-
-
-
-
-
-
-/*let num = 266219; 
-let arr = ('' + num).split('');
-const num = 266219; 
-const arr = ('' + num).split('');
-const reducer = (accumulator, currentValue) => accumulator * currentValue;
-console.log(arr.reduce(reducer));
+/*if(lang == 'ru'){
+	daysOfTheWeekRu.forEach((item) => {
+		console.log(item);
+	})
+}else if (lang == 'en'){
+	daysOfTheWeekEn.forEach((item) => {
+		console.log(item);
+	})
+} else {
+	console.log('Вы ввели не коректное значение');
+}*/
 
 
 
-
-/*for(i = 0; i < arr.length; i++) {
-
+/*switch(lang){
+	case 'ru':
+		daysOfTheWeekRu.forEach((item) => {
+			console.log(item);
+		});
+	break;
+	case 'en':
+		daysOfTheWeekEn.forEach((item) => {
+			console.log(item);
+		});
+	break;
+	default: 
+		console.log('Вы ввели не коректное значение');
 }*/
 
 
 
 
-/*
-let number = ('' + exponentiation).split('');
-let result = arr.reduce(reducer);*/
+
+/*let daysOfTheWeekRu = [['ru'],['Понедельник','Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']],
+	daysOfTheWeekEn = [['en'],['Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']],
+	question = prompt('Введите значение ru или en');
+
+	question.daysOfTheWeekRu == true;
+	question.daysOfTheWeekEn == false;
+
+
+	console.log(question);*/
+
+
+let namePerson = prompt('Введите имя!');
+let name = (namePerson == 'Артем') ? "директор" : (namePerson == 'Максим') ? "преподаватель" : "студент";
+console.log(name);
+
+
+
+
