9fbbb39fb4a535ea94f2e6d7fc505bcbc1855835
Denis Kurilo <kdb2015@mail.ru>
2020-06-10 11:48:03 +0300

    add lesson04 Hard

--- js/script.js
+++ js/script.js
@@ -1,23 +1,11 @@
-/*
-1) Создайте функцию, которая принимает 1 аргумент (название произвольное)
-— Если в качестве аргумента передана не строка - функция оповещает об этом пользователя
-— В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце
-— Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
-
-*/
 function inputOfaLine(data){
-
-	let sum = data.length;
-	console.log(sum)
-	if (typeof data == 'string'){
-		return data.trim();
-
-		// console.log(data.length);
-		
+	if (typeof data == 'string' && data.length >= 30){
+		if(data.length >= 30){
+			return data.substr(0, 30).trim() + '...';
+		}
 	}else {
 		return 'Вы ввели число, введите строку';
 	}
-
 }
 console.log(inputOfaLine('   gdfgsdjgsjdlgjldfjfsdhaslfhakfhL;JFHLJKAsgflakjsgdflaafdfghfgfjksdfksjgdfg1  '));
 
