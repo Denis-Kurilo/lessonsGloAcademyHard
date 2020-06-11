c2d8c62c620d4042e0410d5172ae554c6cfaf9dc
Denis Kurilo <kdb2015@mail.ru>
2020-06-11 09:27:17 +0300

    исправил ошибку в усложненном дз

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function inputOfaLine(data){
 	if (typeof data == 'string' && data.length < 30){
 		console.log(data.trim());
-	}else if( data.length > 30) {
+	}else if( data.length >= 30) {
 		console.log(data.slice(0, 30).trim() + '...');
 	}else{
 		console.log('Вы ввели число, введите строку');
 	}
 }
-inputOfaLine('   sdhfsldhgsdhgksjldhfklsdhfklsjhdfkjls   ');
+inputOfaLine('sdfsdgdfgdfgdfgdfgdfgdddddfsddf  ');
 
 
 
