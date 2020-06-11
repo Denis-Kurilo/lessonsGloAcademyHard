43484d36e284e7a71de292f1f73bd7a9b1bd6a63
Denis Kurilo <kdb2015@mail.ru>
2020-06-11 10:49:09 +0300

    add lesson05 Hard

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-function inputOfaLine(data){
-	if (typeof data == 'string' && data.length < 30){
-		console.log(data.trim());
-	}else if( data.length >= 30) {
-		console.log(data.slice(0, 30).trim() + '...');
-	}else{
-		console.log('Вы ввели число, введите строку');
-	}
-}
-inputOfaLine('sdfsdgdfgdfgdfgdfgdfgdddddfsddf  ');
+/*const arr = ['21','20','12','41','23','43','92'];
 
+for(let i = 0; i < arr.length; i++) {
+	if(arr[i].startsWith('2') || arr[i].startsWith('4')){
+		console.log(arr[i]);
+	}
+}*/
+
+let counter = 1;
+for (let i = 1; i <= 100; i++){
+	console.log(`${i} Делители этого числа: 1 и ${i}`);
+}
 
 
