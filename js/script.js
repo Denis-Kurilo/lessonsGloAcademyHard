1d60d7709608b7cef6352425f172efe7cd5b3d52
Kurilo Denis <webprogram2017@mail.ru>
2020-06-12 19:28:37 +0300

    исправил n в lesson05

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 /*const arr = ['21','20','12','41','23','43','92'];
-
 for(let i = 0; i < arr.length; i++) {
 	if(arr[i].startsWith('2') || arr[i].startsWith('4')){
 		console.log(arr[i]);
@@ -11,7 +10,5 @@ for (let i = 2; i <= 100; i++){
 	for (let n = 2; n < i; n++){
 		if( i % n === 0) continue nextPrime; 
 	}
-	console.log(`${i} - Делители этого числа: 1 и n`);
+	console.log(`Делители этого числа: 1 и ${i}`);
 }
-
-
