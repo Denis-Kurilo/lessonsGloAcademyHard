e7ad0ab3db0dc4454e5a68d2b3ba810fabd7ad0e
Kurilo Denis <webprogram2017@mail.ru>
2020-06-14 19:37:45 +0300

    add lesson06GameBotHard

--- js/script.js
+++ js/script.js
@@ -1,14 +1,46 @@
-/*const arr = ['21','20','12','41','23','43','92'];
-for(let i = 0; i < arr.length; i++) {
-	if(arr[i].startsWith('2') || arr[i].startsWith('4')){
-		console.log(arr[i]);
-	}
-}*/
+let number,
+  count = 3;
 
-nextPrime:
-for (let i = 2; i <= 100; i++){
-	for (let n = 2; n < i; n++){
-		if( i % n === 0) continue nextPrime; 
-	}
-	console.log(`Делители этого числа: 1 и ${i}`);
+function randomInteger(min, max) {
+    return  Math.floor(Math.random() * (max - min)) + 1; 
+    };
+number = randomInteger(1,100);
+
+function youWantToPlay() {
+  if(confirm('Хотели бы сыграть еще?')){
+      window.location.reload();  
+  }else{
+    alert('Завершить игру');
+  }
 }
+
+// console.log(number)
+
+function guessNum() {
+     let num;
+     num = +prompt('Угадайте число от 1 до 100');
+     if(count == 1) {
+        if(alert('Попытки закончились, хотите сыграть еще?')){
+            window.location.reload(); 
+        }
+        return youWantToPlay();
+     } 
+     count--;
+
+     if (num == number) {
+        alert('Поздравляю, Вы угадали!!! ');
+       return youWantToPlay();
+     } else if (num > number && num != '') {
+        alert('Вы ввели число больше! Осталось попыток: ' + count);
+        guessNum();
+     }else if (num < number && num != '') {
+        alert('Вы ввели число меньше! Осталось попыток: ' + count);
+        guessNum();
+     } else if (isNaN(parseFloat(num) && isFinite(num))) {
+        alert('Введите число');
+        guessNum();
+     }else{
+       alert('Завершить игру')
+     }
+}
+guessNum();
