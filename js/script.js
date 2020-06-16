4969c56e2fe5a911369209cf0c255d39223ad749
Kurilo Denis <webprogram2017@mail.ru>
2020-06-17 00:19:04 +0300

    исправил счетчик

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 const game = function(){
     let min = 0,
         max = 100,
-        count = 3,
+        count = 10,
         rundomNum;
 
     rundomNum = Math.floor(Math.random() * (max - min)) + 1;
     // console.log(rundomNum);
-    
-    function counts(){
-    return function(){
-        if(count == 1) {
-            alert('Попытки закончились');
-            return youWantToPlay();
-         }
-          return --count;   
-        }
-    }
-    let counter = counts();
 
     function youWantToPlay() {
       if(confirm('Хотели бы сыграть еще?')){
@@ -25,17 +14,25 @@ const game = function(){
         alert('Завершить игру');
       }
     }
-    
     return function guessNum() {
         let num = +prompt('Угадайте число от 1 до 100');
+
+        if(count == 1) {
+        if(alert('Попытки закончились!!!')){
+            window.location.reload(); 
+        }
+            return youWantToPlay();
+         } 
+         count--;
+
          if (num == rundomNum) {
             alert('Поздравляю, Вы угадали!!!');
             return youWantToPlay();
-         } else if (num > rundomNum && num != '') {
-            alert(`Загаданное число меньше, осталось попыток ${counter()}` );
+         } else if (num >= rundomNum && num != '') {
+            alert('Загаданное число меньше, осталось попыток' + count );
             guessNum();
-         }else if (num < rundomNum && num != '') {
-            alert(`Загаданное число больше, осталось попыток ${counter()}`);
+         }else if (num <= rundomNum && num != '') {
+            alert(`Загаданное число больше, осталось попыток ${count}`);
             guessNum();
          } else if (isNaN(parseFloat(num) && isFinite(num))) {
             alert('Введите число');
@@ -44,7 +41,8 @@ const game = function(){
            alert('Завершить игру')
          }
         }
-         return guessNum();
+        guessNum();
+         console.dir(guessNum())
     }
 let startGame = game();
 startGame();
