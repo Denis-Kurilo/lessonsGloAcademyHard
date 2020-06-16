c8e830f90d822f43e3d77785a01c43a43a9add42
Denis Kurilo <kdb2015@mail.ru>
2020-06-16 10:37:20 +0300

    исправил замыкание

--- js/script.js
+++ js/script.js
@@ -1,40 +1,45 @@
-let number,
-  count = 3;
-
 function randomInteger(min, max) {
-    return  Math.floor(Math.random() * (max - min)) + 1; 
-    };
-number = randomInteger(1,100);
+    return function(){
+       return Math.floor(Math.random() * (max - min)) + 1;
+
+    }  
+};
+let userRandom = randomInteger(1,100);
+let userRun = userRandom();
+console.log(userRun)
+
+function count(){
+    let count = 3;
+        return function(){
+            if(count == 1) {
+                alert('Попытки закончились');
+                return youWantToPlay();;
+             }
+              return --count;   
+        }
+}
+let counter = count();
 
 function youWantToPlay() {
   if(confirm('Хотели бы сыграть еще?')){
-      window.location.reload();  
+      return window.location.reload();  
   }else{
     alert('Завершить игру');
   }
 }
 
-// console.log(number)
-
 function guessNum() {
-     let num;
-     num = +prompt('Угадайте число от 1 до 100');
-     if(count == 1) {
-        if(alert('Попытки закончились, хотите сыграть еще?')){
-            window.location.reload(); 
-        }
-        return youWantToPlay();
-     } 
-     count--;
+    let num;
+    num = +prompt('Угадайте число от 1 до 100');
 
-     if (num == number) {
+     if (num == userRun) {
         alert('Поздравляю, Вы угадали!!! ');
-       return youWantToPlay();
-     } else if (num > number && num != '') {
-        alert('Вы ввели число больше! Осталось попыток: ' + count);
+        return youWantToPlay();
+     } else if (num > userRun && num != '') {
+        alert('Вы ввели число больше! Осталось попыток: ' + counter());
         guessNum();
-     }else if (num < number && num != '') {
-        alert('Вы ввели число меньше! Осталось попыток: ' + count);
+     }else if (num < userRun && num != '') {
+        alert('Вы ввели число меньше! Осталось попыток: ' + counter());
         guessNum();
      } else if (isNaN(parseFloat(num) && isFinite(num))) {
         alert('Введите число');
