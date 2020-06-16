cbb045132bedf85429f10653b6a678f86d57014b
Kurilo Denis <webprogram2017@mail.ru>
2020-06-16 21:11:10 +0300

    исправил урок 6

--- js/script.js
+++ js/script.js
@@ -1,51 +1,51 @@
-function randomInteger(min, max) {
+const game = function(){
+    let min = 0,
+        max = 100,
+        count = 3,
+        rundomNum;
+
+    rundomNum = Math.floor(Math.random() * (max - min)) + 1;
+    // console.log(rundomNum);
+    
+    function counts(){
     return function(){
-       return Math.floor(Math.random() * (max - min)) + 1;
-
-    }  
-};
-let userRandom = randomInteger(1,100);
-let userRun = userRandom();
-console.log(userRun)
-
-function count(){
-    let count = 3;
-        return function(){
-            if(count == 1) {
-                alert('Попытки закончились');
-                return youWantToPlay();;
-             }
-              return --count;   
+        if(count == 1) {
+            alert('Попытки закончились');
+            return youWantToPlay();
+         }
+          return --count;   
         }
-}
-let counter = count();
+    }
+    let counter = counts();
 
-function youWantToPlay() {
-  if(confirm('Хотели бы сыграть еще?')){
-      return window.location.reload();  
-  }else{
-    alert('Завершить игру');
-  }
-}
-
-function guessNum() {
-    let num;
-    num = +prompt('Угадайте число от 1 до 100');
-
-     if (num == userRun) {
-        alert('Поздравляю, Вы угадали!!! ');
-        return youWantToPlay();
-     } else if (num > userRun && num != '') {
-        alert('Вы ввели число больше! Осталось попыток: ' + counter());
-        guessNum();
-     }else if (num < userRun && num != '') {
-        alert('Вы ввели число меньше! Осталось попыток: ' + counter());
-        guessNum();
-     } else if (isNaN(parseFloat(num) && isFinite(num))) {
-        alert('Введите число');
-        guessNum();
-     }else{
-       alert('Завершить игру')
-     }
-}
-guessNum();
+    function youWantToPlay() {
+      if(confirm('Хотели бы сыграть еще?')){
+          return window.location.reload();  
+      }else{
+        alert('Завершить игру');
+      }
+    }
+    
+    return function guessNum() {
+        let num = +prompt('Угадайте число от 1 до 100');
+         if (num == rundomNum) {
+            alert('Поздравляю, Вы угадали!!!');
+            return youWantToPlay();
+         } else if (num > rundomNum && num != '') {
+            alert(`Загаданное число меньше, осталось попыток ${counter()}` );
+            guessNum();
+         }else if (num < rundomNum && num != '') {
+            alert(`Загаданное число больше, осталось попыток ${counter()}`);
+            guessNum();
+         } else if (isNaN(parseFloat(num) && isFinite(num))) {
+            alert('Введите число');
+            guessNum();
+         }else{
+           alert('Завершить игру')
+         }
+        }
+         return guessNum();
+    }
+let startGame = game();
+startGame();
+// console.dir(startGame)
