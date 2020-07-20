f726a311c25212d6d121f231fefb5786c9939abf
Denis Kurilo <kdb2015@mail.ru>
2020-07-20 17:10:08 +0300

    lesson07

--- js/script.js
+++ js/script.js
@@ -1,51 +1,19 @@
-function randomInteger(min, max) {
-    return function(){
-       return Math.floor(Math.random() * (max - min)) + 1;
+const daysOfTheWeek = () => {
+  const weeks = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
+  toDay = new Date().getDay() - 1;
 
-    }  
-};
-let userRandom = randomInteger(1,100);
-let userRun = userRandom();
-console.log(userRun)
-
-function count(){
-    let count = 3;
-        return function(){
-            if(count == 1) {
-                alert('Попытки закончились');
-                return youWantToPlay();;
-             }
-              return --count;   
-        }
-}
-let counter = count();
-
-function youWantToPlay() {
-  if(confirm('Хотели бы сыграть еще?')){
-      return window.location.reload();  
-  }else{
-    alert('Завершить игру');
-  }
-}
-
-function guessNum() {
-    let num;
-    num = +prompt('Угадайте число от 1 до 100');
-
-     if (num == userRun) {
-        alert('Поздравляю, Вы угадали!!! ');
-        return youWantToPlay();
-     } else if (num > userRun && num != '') {
-        alert('Вы ввели число больше! Осталось попыток: ' + counter());
-        guessNum();
-     }else if (num < userRun && num != '') {
-        alert('Вы ввели число меньше! Осталось попыток: ' + counter());
-        guessNum();
-     } else if (isNaN(parseFloat(num) && isFinite(num))) {
-        alert('Введите число');
-        guessNum();
-     }else{
-       alert('Завершить игру')
+  for (let i = 0; i < weeks.length; i++) {
+   if (i == toDay) {
+     if (weeks[i] == 'Суббота' || weeks[i] == 'Воскресенье') {
+       document.write(`<p><b>${toDay[i]}</b></p>`);
+     } else {
+       document.write(`<p><b>${weeks[i]}</b></p>`);
      }
-}
-guessNum();
+   } else if (weeks[i] == 'Суббота' || weeks[i] == 'Воскресенье') {
+     document.write(`<p><i>${weeks[i]}</i></p>`);
+   } else {
+     document.write(`<p>${weeks[i]}</p>`);
+   }
+ };
+};
+daysOfTheWeek();
