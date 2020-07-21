d17743a6b1af5c2c4970941c59728f1d0040fd94
Denis Kurilo <kdb2015@mail.ru>
2020-07-21 15:21:05 +0300

    lesson18

--- js/script.js
+++ js/script.js
@@ -1,19 +1,55 @@
-const daysOfTheWeek = () => {
-  const weeks = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
-  toDay = new Date().getDay() - 1;
+let imgAirplane = document.querySelector('.imgAirplane'),
+  parachutel = document.querySelector('.parachutel'),
+  btn = document.querySelector('.btnPlay'),
+  reset = document.querySelector('.reset');
 
-  for (let i = 0; i < weeks.length; i++) {
-   if (i == toDay) {
-     if (weeks[i] == 'Суббота' || weeks[i] == 'Воскресенье') {
-       document.write(`<p><b>${toDay[i]}</b></p>`);
-     } else {
-       document.write(`<p><b>${weeks[i]}</b></p>`);
-     }
-   } else if (weeks[i] == 'Суббота' || weeks[i] == 'Воскресенье') {
-     document.write(`<p><i>${weeks[i]}</i></p>`);
-   } else {
-     document.write(`<p>${weeks[i]}</p>`);
-   }
- };
+let count = 0;
+let interval;
+let stopAnimate = true;
+let flyAnimate = () => {
+    interval = requestAnimationFrame(flyAnimate);
+    count++;
+    console.log(count)
+    if(count < 400){
+      imgAirplane.style.left = count + 'px'; 
+      parachutel.style.top = count + 'px'; 
+      parachutel.style.display = 'block';
+      if(count > 400){
+        let setInt = setInterval(() => {
+          parachutel.style.top = count + 'px'; 
+          parachutel.style.display = 'block';
+        }, 2);
+      }
+    }else if(count < 600){
+      imgAirplane.style.left = count + 'px';
+    } else{
+      cancelAnimationFrame(interval);
+      
+    }
 };
-daysOfTheWeek();
+
+btn.addEventListener('click', () => {
+  if(stopAnimate){
+    interval = requestAnimationFrame(flyAnimate); 
+    stopAnimate = false;
+    btn.textContent = 'Стоп';
+  } else{
+    stopAnimate = true;
+    btn.textContent = 'Вызов анимации';
+    cancelAnimationFrame(interval);
+  }
+});
+
+reset.addEventListener('click', () => {
+  count = 0;
+  if(stopAnimate){
+    stopAnimate = true;
+    btn.textContent = 'Вызов анимации';
+  }else{
+    stopAnimate = true;
+    btn.textContent = 'Вызов анимации';
+    cancelAnimationFrame(interval);
+  }
+  imgAirplane.style.left = count + 'px'; 
+  parachutel.style.display = 'none';
+});
